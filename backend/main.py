f96aa7cf226d9309c4d27f312fa5085f8692c5e9
Badal Gupta <107276812+Cloudyboiii@users.noreply.github.com>
2026-09-07 12:31:14 -0400

    Connect production frontend and backend

--- backend/main.py
+++ backend/main.py
@@ -14,6 +14,7 @@ app = FastAPI(title="TalkToData API", version="1.0.0")
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://talk-to-data-gold.vercel.app",
 ]
 
 prod_url = os.getenv("FRONTEND_URL")
